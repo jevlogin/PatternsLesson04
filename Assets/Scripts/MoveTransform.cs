add1b3d4dbb69c36aebfbc0a11e82cb4a2f268b5
jevlogin <jev.login@mail.ru>
2020-12-11 08:34:37 +0300

    подчистили апдейт

--- Assets/Scripts/MoveTransform.cs
+++ Assets/Scripts/MoveTransform.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 
+
 namespace JevLogin
 {
     internal class MoveTransform : IMove
@@ -8,14 +9,14 @@ namespace JevLogin
         private Vector3 _move;
 
         public float Speed { get; protected set; }
-        
+
         public MoveTransform(Transform transform, float speed)
         {
             _transform = transform;
             Speed = speed;
         }
 
-        public void Move(float horizontal, float vertical,  float deltaTime)
+        public void Move(float horizontal, float vertical, float deltaTime)
         {
             var speed = deltaTime * Speed;
             _move.Set(horizontal * speed, vertical * speed, 0.0f);
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,33 +10,16 @@ namespace JevLogin
         [SerializeField] private Rigidbody2D _bullet;
         [SerializeField] private Transform _barrel;
         [SerializeField] private float _force;
-        private Camera _camera;
-        private Ship _ship;
+        private MoveTransform _moveTransform;
 
         private void Start()
         {
-            _camera = Camera.main;
-            var moveTransform = new AccelerationMove(transform, _speed, _acceleration);
-            var rotation = new RotationShip(transform);
-            _ship = new Ship(moveTransform, rotation);
+            _moveTransform = new MoveTransform(transform, _speed);
         }
 
         private void Update()
         {
-            var direction = Input.mousePosition - _camera.WorldToScreenPoint(transform.position);
-            _ship.Rotation(direction);
-            
-            _ship.Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), Time.deltaTime);
-
-            if (Input.GetKeyDown(KeyCode.LeftShift))
-            {
-                _ship.AddAcceleration();
-            }
-
-            if (Input.GetKeyUp(KeyCode.LeftShift))
-            {
-                _ship.RemoveAcceleration();
-            }
+            _moveTransform.Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), Time.deltaTime);
 
             if (Input.GetButtonDown("Fire1"))
             {
